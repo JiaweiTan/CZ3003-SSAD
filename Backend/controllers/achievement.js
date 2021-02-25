50141aff8f0fd8db25e4dee25cb5e81182c41897
JiaweiTan <JiaweiTan1996@gmail.com>
2021-02-25 23:10:16 +0800

    Added achievement endpoint

--- Backend/controllers/achievement.js
+++ Backend/controllers/achievement.js
@@ -1,6 +1,36 @@
 const { User } = require("../models/user");
 const { Achievement } = require("../models/achievement");
 
+exports.GetAllAchievement = async (req, res) => {
+  try {
+    var sortBy = req.query?.sortBy || "default";
+    sortBy = sortBy == "default" ? "achievement_desc" : sortBy[0];
+    var sortDesc = req.query?.sortDesc || "default";
+    sortDesc = sortDesc == "default" ? "" : sortDesc[0];
+    sortDesc === "true"
+      ? (sortBy = { [sortBy]: -1 })
+      : (sortBy = { [sortBy]: 1 });
+    var paginationInfo = {
+      page: req.query.page,
+      itemsPerPage: req.query?.itemsPerPage - 0,
+      sortBy: sortBy,
+      sortDesc: sortDesc,
+    };
+    var achievements = await Achievement.find()
+      .skip((paginationInfo.page - 1) * paginationInfo.itemsPerPage)
+      .limit(paginationInfo.itemsPerPage)
+      .sort(paginationInfo.sortBy)
+      .exec();
+    var count = await Achievement.find().countDocuments({});
+    var achievementData = { achievements, count };
+    return res.status(200).json(achievementData);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json(e).end();
+  }
+};
+
+
 exports.GetAchievement = (req, res, next) => {
   try {
     Achievement.findOne(
